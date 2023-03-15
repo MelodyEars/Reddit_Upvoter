5e966f6b8519bba3dd0e5c5337a1817fb16585a5
ArtDanger <danya221299@gmail.com>
2023-03-15 15:38:10 +0200

    Changes by King

--- Uprove_TG_Bot/TG_bot/work_PROCESS.py
+++ Uprove_TG_Bot/TG_bot/work_PROCESS.py
@@ -25,12 +25,12 @@ class StructData(NamedTuple):
     comments_int: int
 
 
-async def run_process_and_reply_after(message: types.Message, data):
+async def run_process_and_reply_after(message: types.Message, data: StructData):
     logger.info("runner process")
 
-    reddit_link = data['reddit_link']
-    upvote_int = data['upvote_int']
-    comments_int = data['comments_int']
+    reddit_link = data.reddit_link
+    upvote_int = data.upvote_int
+    comments_int = data.comments_int
 
     proc = Process(target=start_reddit_work, args=(reddit_link, upvote_int, comments_int))
     proc.start()
