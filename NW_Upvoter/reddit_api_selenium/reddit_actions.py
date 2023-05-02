13c4adf6a709be9e7276575dfba47a297f6ef239
ArtDanger <danya221299@gmail.com>
2023-05-02 18:23:11 +0300

    gpt4 optimization new up selenium

--- NW_Upvoter/reddit_api_selenium/reddit_actions.py
+++ NW_Upvoter/reddit_api_selenium/reddit_actions.py
@@ -1,8 +1,4 @@
-# import time
-from threading import Thread
 import time
-
-# from pywinauto import Application
 
 from loguru import logger
 from selenium.common import ElementClickInterceptedException
@@ -10,33 +6,25 @@ from selenium.common import ElementClickInterceptedException
 from BASE_Reddit.BaseReddit import BaseReddit
 from BASE_Reddit.exceptions import CookieInvalidException, PostDeletedException
 from Settings_Selenium.SupportSelenium import BrowserCookie
-# from SWITCHer_window import auto_focus_every_30
 
 
 class RedditWork(BaseReddit):
     def __init__(self, path_cookie=str, link=str, proxy=None):
-        super(__class__, self).__init__()
+        super().__init__()
 
         self.client_cookie = BrowserCookie
         self.proxy = proxy
         self.link = link
         self.cookie_path = path_cookie
-        self.thread = Thread
 
     def __enter__(self):
         self.DRIVER = self.run_driver(proxy=self.proxy)
-        # browser_pid = self.DRIVER.browser_pid
-
-        # self.thread = Thread(target=auto_focus_every_30, args=(browser_pid, ))
-        # self.thread.start()
-
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_type or exc_val or exc_tb:
             self.DRIVER.save_screenshot("UpvoterMistake.png")
         self.DRIVER.quit()
-        # self.thread.join()
 
     def _deleted_post(self):
         self._baned_account()
@@ -48,69 +36,46 @@ class RedditWork(BaseReddit):
             raise PostDeletedException("this post has been removed")
 
     def attend_link(self):
-        # self._activation_window()  # ___________________________________________________________ activation
         logger.info("Check cookie valid!")
-        # self.DRIVER.delete_all_cookies()
         self.client_cookie = BrowserCookie(driver=self.DRIVER, path_filename=self.cookie_path)
 
         if self.client_cookie.are_valid():
             self.client_cookie.preload()
             logger.info("Attend link!")
             self.DRIVER.get(self.link)
-            # self.DRIVER.refresh()
-            # self.DRIVER.reconnect()
         else:
             raise CookieInvalidException("Cookie invalid")
 
     def _find_popups(self):
         logger.info("Find Popups!")
-        # use Reddit in browser
+
         if self.click_element(value='//a[contains(text(), "Browse Reddit")]', wait=0.2):
             self.wait_load_webpage()
 
-        # THen content 18+
         if self.elem_exists('//h3[contains(text(), "You must be 18+")]', wait=0.2):
             logger.info('You must be 18+')
             self.click_element('//button[contains(text(), "Yes")]')
             self.wait_load_webpage()
 
-        # when we watch on the first time on the network
         self.select_interests()
-
-        # asks to continue when you visit a site with a post
         self._button_continue()
 
     def _previously_upvote(self, wait):
-        # click button upvote
         if self.click_element(
                 value='//div[@data-test-id="post-content"]//button[@data-click-id="upvote" and @aria-pressed="false"]',
                 wait=wait):
             logger.info("Відбувся клік по апвоуту!")
 
             time.sleep(2)
-            # check if red button
             if self.elem_exists('//div[@data-test-id="post-content"]//i[contains(@class, "icon icon-upvote_fill ")]',
                                 wait=4):
                 logger.info("Клік червоний проходимо далі!")
-                # success
-                return
             else:
-                # repeats actions
                 logger.error("Клік був, але кнопка апвоуту і досі прозора!")
                 return self.upvote()
 
         else:
             logger.error("Кліку по апвоуту не було!")
-            # the upvote has already been made
-            if self.elem_exists(
-                    value='//div[@data-test-id="post-content"]//button[@data-click-id="upvote" and @aria-pressed="true"]',
-                    wait=1):
-                logger.info("Upvote exists!")
-                return
-            else:
-                logger.error("Щось пішло не запланом, мабуть з'явились меню вибору інтересів!")
-                self._find_popups()
-                self._previously_upvote(wait)
 
     def upvote(self, wait=4):
         try:
