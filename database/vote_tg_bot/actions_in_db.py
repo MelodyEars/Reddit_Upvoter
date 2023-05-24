69a928024823913b72eee4508607f1ca07f13392
albedo <danya221299@gmail.com>
2023-05-24 17:10:01 +0300

    experiment

--- database/vote_tg_bot/actions_in_db.py
+++ database/vote_tg_bot/actions_in_db.py
@@ -27,7 +27,7 @@ def db_exist_record_link_account(link_id, cookie_id) -> (bool, WorkAccountWithLi
 
 def db_get_random_account_with_0() -> list[Cookie]:
     with db:
-        cookies_db_objs = Cookie.select()  # .where((Cookie.is_selected == False) & (Cookie.ban.is_null(True)))
+        cookies_db_objs = Cookie.select().order_by(Cookie.id)  # .where((Cookie.is_selected == False) & (Cookie.ban.is_null(True)))
 
     return cookies_db_objs
 
