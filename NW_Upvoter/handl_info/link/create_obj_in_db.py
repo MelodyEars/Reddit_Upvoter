bc9700d4131dfd6a40c51f9b218c8dd9caf37dc7
albedo <danya221299@gmail.com>
2023-05-29 01:01:05 +0300

    upd models nw

--- NW_Upvoter/handl_info/link/create_obj_in_db.py
+++ NW_Upvoter/handl_info/link/create_obj_in_db.py
@@ -1,5 +1,7 @@
 import re
 
+from loguru import logger
+
 from NW_Upvoter.db_tortories_orm.query.link import db_get_or_create_link_obj
 from NW_Upvoter.handl_info import get_reveddit_link
 from base_exception import ThisLinkIsNotPostException
@@ -19,6 +21,7 @@ async def analizated_link(message, reddit_link, upvote_int):
 
     if match_pattern_reddit_url(reddit_link):
         reveddit_url = await get_reveddit_link(reddit_link)
+        logger.info(f"reveddit_url: {reveddit_url}")
         link_obj, created = await db_get_or_create_link_obj(reddit_link, who_posted, sub, upvote_int, reveddit_url)
 
         if created:
