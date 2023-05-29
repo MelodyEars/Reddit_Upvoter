3c3bd3b0a56a9b662c092427837cec3d692c5402
albedo <danya221299@gmail.com>
2023-05-29 11:58:55 +0300

    fix xpath reveddit

--- NW_Upvoter/reddit_api_selenium/reddit_actions.py
+++ NW_Upvoter/reddit_api_selenium/reddit_actions.py
@@ -95,7 +95,7 @@ class RedditWork(BaseReddit):
         self.DRIVER.get(reveddit_link)
         self.elem_exists(by=By.TAG_NAME, value='body')
         self.click_element('//div[@id="genericModal"]//a[@class="pointer"]', wait=0.2)
-        if self.elem_exists(f'//a[contains(@href, "{self.link}"', wait=1):
+        if self.elem_exists(f'//a[contains(@href, "{self.link}")]', wait=1):
             logger.info("Post not removed!")
             return
         else:
