5f95ca0a4a348768826c1e1d53bc5bbd20f1242d
albedo <danya221299@gmail.com>
2023-06-06 16:35:26 +0300

    counter

--- NW_Upvoter/handl_info/count_upvoter.py
+++ NW_Upvoter/handl_info/count_upvoter.py
@@ -3,14 +3,14 @@ def adder_count_upvote(upvote_int):
     if upvote_int < 10:
         pass
     elif upvote_int < 10:
-        upvote_int += 1
+        upvote_int += 2
     elif upvote_int < 20:
         upvote_int += 2
     elif upvote_int < 30:
-        upvote_int += 3
+        upvote_int += 2
     elif upvote_int < 40:
-        upvote_int += 4
+        upvote_int += 2
     else:
-        upvote_int += 5
+        upvote_int += 2
 
     return upvote_int
