2145f2c0e420ff8243c7cddf042410bdc8230ea9
ArtDanger <danya221299@gmail.com>
2023-06-21 13:58:29 +0300

    upd selenium stealth

--- SETTINGS.py
+++ SETTINGS.py
@@ -7,9 +7,8 @@ from work_fs.PATH import path_near_exefile
 from work_fs.sertificate_db.path_to_sertificate import path_to_sertificate
 
 mine_project = True
-incubator = True
+incubator = False
 COUNT_BOT = 20
-
 
 # ____________________________________________________________________ DATABASE
 # set up db_tg_bot
--- selenium_stealth/utils.py
+++ selenium_stealth/utils.py
@@ -9,8 +9,9 @@ from selenium.webdriver import Chrome as Driver
 #     evaluateOnNewDocument(
 #         driver, Path(__file__).parent.joinpath("js/utils.js").read_text()
 #     )
-#
-def with_utils(driver: Driver, **kwargs) -> None:
-    utils_path = path_in_exefile(Path(__file__).parent / "js/utils.js")
-    evaluateOnNewDocument(driver, utils_path.read_text())
 
+
+def with_utils(driver: Driver, **kwargs) -> None:
+    js_path = path_in_exefile()
+    utils_path = js_path / "js" / 'utils.js'
+    evaluateOnNewDocument(driver, utils_path.read_text())
